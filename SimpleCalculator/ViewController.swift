44aabcb56db750442c0f890ed77fdd79c537eea2
ibrahim Burak Ezdemir <ibrahimezdemir@gmail.com>
2022-01-14 18:22:26 +0300

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Burak on 14.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
